eae89848f8a857e771139c363c838a3dee96fff7
mmfallacy <monasterialma@gmail.com>
2023-09-19 11:22:58 +0800

    feat: migrate to lazy

--- .config/nvim/lua/mmfallacy/plugins.lua
+++ .config/nvim/lua/mmfallacy/plugins.lua
@@ -1,154 +1,139 @@
-vim.cmd [[packadd packer.nvim]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
 
-function plugins(use)
-    use 'wbthomason/packer.nvim'
-
-    -- Color Schemes
-    use {
-        {
-            'projekt0n/github-nvim-theme',
-            -- config = require'mmfallacy.themes.github'
-        },
-        {
-            'navarasu/onedark.nvim',
-            -- config = require 'mmfallacy.themes.onedark'
-        },
-        {
-            'AlexvZyl/nordic.nvim',
-            -- config = require 'mmfallacy.themes.nordic'
-        },
-        {
-            'catppuccin/nvim',
-            as = 'catppuccin',
-            config = require 'mmfallacy.themes.catppuccin'
-        }
-    }
-
-    -- Telescope
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        requires = { { 'nvim-lua/plenary.nvim' } },
+local plugins = {
+    -- Themes
+    {
+        'catppuccin/nvim',
+        name = 'catppuccin',
+        priority = 1000,
+        config = require 'mmfallacy.themes.catppuccin'
+    },
+    {
+        'nvim-telescope/telescope.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim' },
         config = require 'mmfallacy.configs.telescope'
-    }
-    -- Nvim-Tree
-    use {
+    },
+    {
         'nvim-tree/nvim-tree.lua',
-        requires = {
-            'nvim-tree/nvim-web-devicons', -- optional
-        },
         config = require 'mmfallacy.configs.nvim-tree'
-    }
+    },
 
     -- Syntax analyzers
-    use {
-        'nvim-treesitter/nvim-treesitter', run = ':TSUpdate',
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
         config = require 'mmfallacy.configs.treesitter'
-    }
+    },
 
-    use {
+    {
         'norcalli/nvim-colorizer.lua',
         config = function() require "colorizer".setup() end
-    }
-
-    use {
+    },
+    {
         'AndrewRadev/tagalong.vim',
         config = function()
             vim.cmd [[
                 let g:tagalong_additional_filetypes = ['svelte']
             ]]
         end
-    }
+    },
 
-    use {
+    {
         'folke/todo-comments.nvim',
-        requires = { { 'nvim-lua/plenary.nvim' } },
-        config = function() require 'todo-comments'.setup() end
-    }
+        dependencies = { 'nvim-lua/plenary.nvim' },
 
-    use {
+        config = function() require 'todo-comments'.setup() end
+    },
+
+    {
         'echasnovski/mini.comment',
         config = require 'mmfallacy.configs.comment'
-    }
+    },
 
-    use {
+    {
         'echasnovski/mini.surround',
         config = function() require 'mini.surround'.setup() end
-    }
+    },
 
-    use {
+    {
         'echasnovski/mini.cursorword',
         config = function() require 'mini.cursorword'.setup() end
-    }
+    },
 
-    use {
+    {
         'echasnovski/mini.pairs',
         config = require 'mmfallacy.configs.autopairs'
-    }
+    },
 
-    use {
+    {
         'lukas-reineke/indent-blankline.nvim',
         config = require 'mmfallacy.configs.indents'
-    }
+    },
 
-    use {
+    {
         'echasnovski/mini.tabline',
         config = function() require 'mini.tabline'.setup() end
-    }
+    },
 
 
     -- Lualine
-    use {
+    {
         'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
+        dependencies = { 'kyazdani42/nvim-web-devicons', opt = true }
+    },
 
-    -- LSP
-    use {
+    -- LSP and Snippets
+    {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
         "neovim/nvim-lspconfig",
-    }
-
-    use {
+    },
+    {
         "jose-elias-alvarez/null-ls.nvim",
-        requires = 'nvim-lua/plenary.nvim',
-    }
-
-    -- Snippet
-    use 'rafamadriz/friendly-snippets'
-    use 'L3MON4D3/LuaSnip'
-    use {
+        dependencies = { 'nvim-lua/plenary.nvim' },
+    },
+    { 'rafamadriz/friendly-snippets' },
+    { 'L3MON4D3/LuaSnip' },
+    {
         'hrsh7th/nvim-cmp',
-        requires = 'L3MON4D3/LuaSnip',
+        dependencies = { 'L3MON4D3/LuaSnip' },
         config = require 'mmfallacy.configs.cmp',
-    }
-    use {
+    },
+    {
         'hrsh7th/cmp-buffer',
-        requires = 'hrsh7th/nvim-cmp',
-    }
-    use {
+        dependencies = { 'hrsh7th/nvim-cmp' },
+    },
+    {
         'saadparwaiz1/cmp_luasnip',
-        requires = 'hrsh7th/nvim-cmp',
-    }
-    use {
+        dependencies = { 'hrsh7th/nvim-cmp' },
+    },
+    {
         'hrsh7th/cmp-nvim-lsp',
-        requires = 'hrsh7th/nvim-cmp',
-    }
-    use {
+        dependencies = { 'hrsh7th/nvim-cmp' },
+    },
+    {
         'hrsh7th/cmp-path',
-        requires = 'hrsh7th/nvim-cmp',
-    }
-
-    -- Git Integration
-    use {
+        dependencies = { 'hrsh7th/nvim-cmp' },
+    },
+    {
         'lewis6991/gitsigns.nvim',
         config = function() require 'gitsigns'.setup() end
-    }
-
-
-    use {
+    },
+    {
         'NoahTheDuke/vim-just',
     }
-end
+}
 
-return require 'packer'.startup(plugins)
+require 'lazy'.setup(plugins)
+-- return require 'packer'.startup(plugins)
