1cd9d4eaa379c8ad1c54efac2d10d43a61e67329
mmfallacy <monasterialma@gmail.com>
2023-10-01 21:15:29 +0800

    nvim: revert undotree

--- .config/nvim/lua/mmfallacy/plugins.lua
+++ .config/nvim/lua/mmfallacy/plugins.lua
@@ -197,14 +197,6 @@ local plugins = {
             ]]
         end
     },
-    {
-        "jiaoshijie/undotree",
-        dependencies = "nvim-lua/plenary.nvim",
-        config = true,
-        keys = { -- load the plugin only when using it's keybinding:
-            { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
-        },
-    }
 }
 
 require 'lazy'.setup(plugins)
