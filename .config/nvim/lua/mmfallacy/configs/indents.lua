fb38bfa4a7992778f1b65cc145df3353cd69616d
mmfallacy <monasterialma@gmail.com>
2023-10-02 21:28:06 +0800

    nvim: fix indent blankline v3 deprecated options

--- .config/nvim/lua/mmfallacy/configs/indents.lua
+++ .config/nvim/lua/mmfallacy/configs/indents.lua
@@ -1,10 +1,7 @@
 return function()
     vim.opt.list = true
-    vim.opt.listchars:append "space:⋅"
     vim.opt.listchars:append "eol:↴"
 
     require("ibl").setup {
-        show_end_of_line = true,
-        space_char_blankline = " ",
     }
 end
