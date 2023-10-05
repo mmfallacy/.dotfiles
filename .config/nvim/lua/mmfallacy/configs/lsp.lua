6efc96ec88caf89e2f9ea6403f3be97bac9124d9
mmfallacy <monasterialma@gmail.com>
2023-10-05 12:04:34 +0800

    nvim: disable lsp on attach aucmd to give way for conform's format aucmd with lsp fallback

--- .config/nvim/lua/mmfallacy/configs/lsp.lua
+++ .config/nvim/lua/mmfallacy/configs/lsp.lua
@@ -1,94 +1,116 @@
-require "mason".setup()
-require "mason-lspconfig".setup({
+require("mason").setup()
+require("mason-lspconfig").setup {
     ensure_installed = {
-        'tsserver',
-        'lua_ls',
-        'rust_analyzer',
-        'emmet_ls',
-        'denols',
-        'svelte',
-        'pyright',
-        'tailwindcss',
-        'eslint',
-        'jdtls',
-        'typst_lsp'
-    }
-})
+        "tsserver",
+        "lua_ls",
+        "rust_analyzer",
+        "emmet_ls",
+        "denols",
+        "svelte",
+        "pyright",
+        "tailwindcss",
+        "eslint",
+        "jdtls",
+        "typst_lsp",
+    },
+}
 
 local lsp = require "lspconfig"
-local caps = require 'cmp_nvim_lsp'.default_capabilities()
+local caps = require("cmp_nvim_lsp").default_capabilities()
 
 local on_attach_base = function(client, bufnr)
     local function bufmap(mode, combo, macro)
         vim.keymap.set(mode, combo, macro, { buffer = bufnr, noremap = true, silent = true })
     end
 
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-    bufmap('n', 'K', vim.lsp.buf.hover)
-    bufmap('n', 'gD', vim.lsp.buf.declaration)
-    bufmap('n', '<C-k>', vim.lsp.buf.signature_help)
-    bufmap('n', '<leader>fmt', function() vim.lsp.buf.format { async = true } end)
-    bufmap('n', '<leader>pd', function() vim.diagnostic.open_float { scope = 'cursor' } end)
-    bufmap('n', '<leader>ld', function() vim.diagnostic.open_float { scope = 'line' } end)
-    bufmap('n', '<leader>ca', vim.lsp.buf.code_action)
-    bufmap('n', '<F2>', vim.lsp.buf.rename)
+    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+    bufmap("n", "K", vim.lsp.buf.hover)
+    bufmap("n", "gD", vim.lsp.buf.declaration)
+    bufmap("n", "<C-k>", vim.lsp.buf.signature_help)
+    bufmap("n", "<leader>fmt", function()
+        vim.lsp.buf.format { async = true }
+    end)
+    bufmap("n", "<leader>pd", function()
+        vim.diagnostic.open_float { scope = "cursor" }
+    end)
+    bufmap("n", "<leader>ld", function()
+        vim.diagnostic.open_float { scope = "line" }
+    end)
+    bufmap("n", "<leader>ca", vim.lsp.buf.code_action)
+    bufmap("n", "<F2>", vim.lsp.buf.rename)
 
-    local t = require 'telescope.builtin'
-    bufmap('n', '<leader>wd', t.diagnostics)
-    bufmap('n', '<leader>dd', function() t.diagnostics({ bufnr = 0 }) end)
-    bufmap('n', 'gd', t.lsp_definitions)
-    bufmap('n', 'gt', t.lsp_type_definitions)
-    bufmap('n', 'gr', t.lsp_references)
-    bufmap('n', '<leader>ds', t.lsp_document_symbols)
+    local t = require "telescope.builtin"
+    bufmap("n", "<leader>wd", t.diagnostics)
+    bufmap("n", "<leader>dd", function()
+        t.diagnostics { bufnr = 0 }
+    end)
+    bufmap("n", "gd", t.lsp_definitions)
+    bufmap("n", "gt", t.lsp_type_definitions)
+    bufmap("n", "gr", t.lsp_references)
+    bufmap("n", "<leader>ds", t.lsp_document_symbols)
 end
 
 local on_attach = function(client, bufnr)
-    vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format({timeout_ms = 5000})]]
-    on_attach_base(client, bufnr);
+    -- vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format({timeout_ms = 5000})]]
+    on_attach_base(client, bufnr)
 end
 -- Language Server Setups
-lsp.lua_ls.setup { on_attach = on_attach, capabilities = caps,
+lsp.lua_ls.setup {
+    on_attach = on_attach,
+    capabilities = caps,
     settings = {
         Lua = {
             diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
+                globals = { "vim" },
+            },
+        },
+    },
 }
 lsp.denols.setup {
     on_attach = on_attach,
     capabilities = caps,
-    root_dir = lsp.util.root_pattern('deno.jsonc', 'deno.json'),
+    root_dir = lsp.util.root_pattern("deno.jsonc", "deno.json"),
     settings = {
         deno = {
             enable = true,
             lint = true,
-            config = 'deno.jsonc'
-        }
+            config = "deno.jsonc",
+        },
     },
     filetypes = {
-        "json", "jsonc", "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact",
-        "typescript.tsx"
-    }
+        "json",
+        "jsonc",
+        "javascript",
+        "javascriptreact",
+        "javascript.jsx",
+        "typescript",
+        "typescriptreact",
+        "typescript.tsx",
+    },
 }
 lsp.tsserver.setup {
     on_attach = on_attach_base,
     capabilities = caps,
     single_file_support = false,
-    root_dir = lsp.util.root_pattern("package.json", "tsconfig.json", "tsconfig.jsonc", "package.jsonc")
+    root_dir = lsp.util.root_pattern("package.json", "tsconfig.json", "tsconfig.jsonc", "package.jsonc"),
 }
 lsp.rust_analyzer.setup { on_attach = on_attach, capabilities = caps }
 lsp.emmet_ls.setup {
     on_attach = on_attach,
     capabilities = caps,
     filetypes = {
-        'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'svelte'
-    }
+        "html",
+        "typescriptreact",
+        "javascriptreact",
+        "css",
+        "sass",
+        "scss",
+        "svelte",
+    },
 }
 lsp.svelte.setup {
     on_attach = on_attach_base,
-    capabilities = caps
+    capabilities = caps,
 }
 lsp.tailwindcss.setup {
     on_attach = on_attach,
@@ -131,9 +153,9 @@ null_ls.setup {
         null_ls.builtins.formatting.prettierd.with {
             extra_filetypes = { "svelte" },
             env = {
-                PRETTIERD_LOCAL_PRETTIER_ONLY = 1
-            }
+                PRETTIERD_LOCAL_PRETTIER_ONLY = 1,
+            },
         },
     },
-    on_attach = on_attach
+    on_attach = on_attach,
 }
--- .config/nvim/lua/mmfallacy/plugins.lua
+++ .config/nvim/lua/mmfallacy/plugins.lua
@@ -229,7 +229,17 @@ local plugins = {
     },
     {
         "stevearc/conform.nvim",
-        opts = {}
+        config = function()
+            require "conform".setup {
+                format_on_save = {
+                    timeout_ms = 1000,
+                    lsp_fallback = true
+                },
+                formatters_by_ft = {
+                    lua = { "stylua" }
+                }
+            }
+        end
     }
 }
 
